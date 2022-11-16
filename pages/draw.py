58758e2b08e2d2fb4c640043c48e614d2eeee985
ZheyuW <118230058+ZheyuW@users.noreply.github.com>
2022-11-16 19:00:55 +0100

    Add files via upload

--- pages/draw.py
+++ pages/draw.py
@@ -1,1 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+st.set_page_config(
+    page_title='draw'
+)
+st.title('for drawing')
+st.balloons()
+x = np.arange(0,4*np.pi,0.01)
+choice = st.selectbox('please choose the diagram',['sin','cos','tan'])
+
+if choice == 'sin':
+    y = np.sin(x)
+elif choice == 'cos':
+    y = np.cos(x)
+else:
+    y = np.tan(x)
+
+st.success('hey! Here is your diagram!')
+
+fig, ax = plt.subplots()
+plt.plot(x,y)
+st.pyplot(fig)
